2a3478b567fef6281470d79ee86701885bbc08d0
Nazar <nazar08032003@gmail.com>
2023-03-02 00:47:12 +0200

    admin changes and test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -46,3 +46,11 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+        Тест для перевірки коректності сторінки створення користувача
+        """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
