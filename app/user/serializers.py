02ac47649db34dcd15435bdfdee7b943cafb02d5
Nazar <nazar08032003@gmail.com>
2023-03-06 01:42:34 +0200

    new test

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,9 @@
 """
 Серіалізатори для моделей користувача
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, \
+    authenticate
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -22,3 +24,42 @@ class UserSerializer(serializers.ModelSerializer):
         Створення нового користувача
         """
         return get_user_model().objects.create_user(**validated_data)
+    def update(self, instance, validated_data):
+        """
+        Оновлення користувача, встановлення пароля
+        """
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    """
+    Серіалізатор для авторизації користувача за токенем
+    """
+    email= serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self,attrs):
+        """
+        Перевірка та авторизація користувача
+        """
+        email= attrs.get('email')
+        password = attrs.get('password')
+        user= authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -2,8 +2,10 @@
 Вью для апі користувачів
 """
 
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -11,3 +13,24 @@ class CreateUserView(generics.CreateAPIView):
     Створення нового користувача
     """
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """
+    Створення нового токена для користувача
+    """
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """
+    Оновлення користувача
+    """
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """
+        Отримання та оновлення користувача
+        """
+        return self.request.user
