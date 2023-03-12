f054e90b9adc9f0d5608952877d10de50c0bca30
Nazar <nazar08032003@gmail.com>
2023-03-13 00:30:17 +0200

    ingredient models and tests and admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,7 +5,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User, Recipe, Tag
+from .models import User, Recipe, Tag, Ingredient
 
 
 class UserAdmin(BaseUserAdmin):
@@ -48,3 +48,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
 admin.site.register(Tag)
+admin.site.register(Ingredient)
