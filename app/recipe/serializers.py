0e61e1c17633b27fba9b52cbabc232fb82850699
Nazar <nazar08032003@gmail.com>
2023-03-13 16:14:36 +0200

    update recipe test and serializers

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -80,9 +80,13 @@ class RecipeSerializer(serializers.ModelSerializer):
         Оновлення рецепта
         """
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
         for key, value in validated_data.items():
             setattr(instance, key, value)
         instance.save()
--- app/recipe/tests/test_recip_api.py
+++ app/recipe/tests/test_recip_api.py
@@ -318,3 +318,53 @@ class PrivateRecipeApiTests(TestCase):
             exists = recipe.ingredients.filter(name=ingredient['name'],
                                                user=self.user).exists()
             self.assertTrue(exists)
+
+    def test_create_ingredient_on_update(self):
+        """
+        Тест для перевірки чи можна оновити рецепт з новими інгредієнтами
+        """
+        recipe = create_recipe(user=self.user)
+
+        payload = {
+            'ingredients': [{'name': 'Limes'}],
+        }
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        new_ingredient = Ingredient.objects.get(user=self.user, name='Limes')
+        self.assertIn(new_ingredient, recipe.ingredients.all())
+
+    def test_update_recipe_assign_ingredient(self):
+        """
+        Тест для перевірки чи можна оновити рецепт з наявними інгредієнтами
+        """
+        ingredient_lemon = Ingredient.objects.create(user=self.user,
+                                                     name='Lemon')
+        recipe = create_recipe(user=self.user)
+        recipe.ingredients.add(ingredient_lemon)
+
+        ingredient_limes = Ingredient.objects.create(user=self.user,
+                                                     name='Limes')
+        payload = {
+            'ingredients': [{'name': 'Limes'}],
+        }
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(ingredient_limes, recipe.ingredients.all())
+        self.assertNotIn(ingredient_lemon, recipe.ingredients.all())
+
+    def test_clear_recipe_ingredients(self):
+        """
+        Тест для перевірки чи можна очистити інгредієнти рецепта
+        """
+        ingredient = Ingredient.objects.create(user=self.user, name='Lemon')
+        recipe = create_recipe(user=self.user)
+        recipe.ingredients.add(ingredient)
+
+        payload = {'ingredients': []}
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(recipe.ingredients.count(), 0)
+        self.assertNotIn(ingredient, recipe.ingredients.all())
