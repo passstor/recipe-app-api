fa956f55b9fbbd8b3d5d6ae9fbc7391fcac3b3d5
Nazar <nazar08032003@gmail.com>
2023-03-13 16:23:24 +0200

    refactoring views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,37 +38,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin, ):
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.CreateModelMixin,
+                            mixins.ListModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.DestroyModelMixin, ):
+    """
+    Базовий вью для тегів і інгредієнтів
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """
+        Отримання атрибутів для поточного користувача
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
     """
     Вью для тегів
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        """
-        Отримання тегів для поточного користувача
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet,
-                        mixins.UpdateModelMixin, mixins.DestroyModelMixin,):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """
     Вью для інгредієнтів
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        """
-        Отримання інгредієнтів для поточного користувача
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
